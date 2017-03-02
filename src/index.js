56fced334b9fae14de42c3463e1ac525119987a1
steelydylan <ess_president@me.com>
2017-03-02 19:12:54 +0900

    commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+;(function umd(factory) {
+	'use strict';
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('jquery'));
+	} else {
+		factory(jQuery);
+	}
+}(function imgViewer($) {
+	$.fn.imgViewer = function(){
+
+	}
+}));
