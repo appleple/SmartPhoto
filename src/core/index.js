89b7a9348a71da295240ce899f0903e05ea4e9e3
steelydylan <ess_president@me.com>
2017-04-19 01:13:11 +0900

    fix js

--- src/core/index.js
+++ src/core/index.js
@@ -271,7 +271,6 @@ class smartPhoto extends aTemplate {
     clone.style.height = `${img.offsetHeight}px`;
     clone.style.transform = `translate(${pos.left}px,${pos.top}px) scale(1)`;
     clone.style.zIndex = '101';
-    clone.style.opacity = '0.5';
 
     setTimeout(() => {
       clone.style.transition = 'all .3s ease-out';
