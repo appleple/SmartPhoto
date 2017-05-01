b611b53ef32342ede3a26d08de3cde3a8f28b091
steelydylan <ess_president@me.com>
2017-05-01 23:20:59 +0900

    v0.4.4

--- js/smartphoto.min.js
+++ js/smartphoto.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.4.3
+ *   version: 0.4.4
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
