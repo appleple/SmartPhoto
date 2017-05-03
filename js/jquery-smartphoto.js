68583a67fe8472544cc76b7a50142bbf18e5d99c
steelydylan <ess_president@me.com>
2017-05-03 22:42:10 +0900

    v0.5.5

--- js/jquery-smartphoto.js
+++ js/jquery-smartphoto.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.5.4
+ *   version: 0.5.5
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
