3b70acf604a29317726e3ca80e18f10f814649a2
堀悟大 <godai@horisatorudainoMacBook-Pro.local>
2017-07-20 17:30:13 +0900

    v0.7.4

--- js/smartphoto.min.js
+++ js/smartphoto.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.7.3
+ *   version: 0.7.4
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
