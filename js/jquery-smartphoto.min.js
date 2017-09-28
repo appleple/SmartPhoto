a49b34de4ada877993c309f76128df1841bb37ec
Hori Godai <ess_president@me.com>
2017-09-28 16:41:26 +0900

    v0.7.8

--- js/jquery-smartphoto.min.js
+++ js/jquery-smartphoto.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.7.7
+ *   version: 0.7.8
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
