970b05b3b5dd8ce8cf27f74681b9eb147192bbbb
steelydylan <ess_president@me.com>
2017-12-31 02:04:10 +0900

    bumping version

--- js/smartphoto.js
+++ js/smartphoto.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.0.2
+ *   version: 1.1.0
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
