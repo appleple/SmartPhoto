42b3973540cc126cf00270fc1d15b0b52168f895
Hori Godai <ess_president@me.com>
2018-03-14 15:47:41 +0900

    bumping version

--- js/smartphoto.js
+++ js/smartphoto.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.2.1
+ *   version: 1.2.2
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
