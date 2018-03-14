349af071f5a93c9e631066e1c4e6bb485995df57
Hori Godai <ess_president@me.com>
2018-03-14 16:42:29 +0900

    bumping version

--- js/jquery-smartphoto.min.js
+++ js/jquery-smartphoto.min.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.2.2
+ *   version: 1.2.3
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
