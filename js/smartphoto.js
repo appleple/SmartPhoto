105220fd9b1d4d2f1a6ecce8a47cfef7f0275ee5
Hori Godai <ess_president@me.com>
2018-03-16 14:38:48 +0900

    build:js

--- js/smartphoto.js
+++ js/smartphoto.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.2.3
+ *   version: 1.2.4
  *
  * a-template:
  *   license: MIT (http://opensource.org/licenses/MIT)
